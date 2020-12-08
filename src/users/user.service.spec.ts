fb826b9f2681f46d9f1bb23461986f28069365c1
양우석 <woosuk288@naver.com>
2020-12-08 12:46:31 +0900

    #7.2 Mocking part Two

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
@@ -21,8 +22,11 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -50,6 +54,11 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get(getRepositoryToken(User));
+  });
+
+  describe('createAccount', () => {
+    it('should fail if user exist', () => {});
   });
 
   it('should createAcount', () => {});
