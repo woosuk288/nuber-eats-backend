7cfe4b69f33055b8167d0537e954f63fb7576c59
양우석 <woosuk288@naver.com>
2020-12-10 21:44:37 +0900

    #8.1 JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -20,6 +27,22 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sign', () => {
+    it('should return a sign token', async () => {
+      const ID = 1;
+      const token = await service.sign(ID);
+      expect(typeof token).toBe('string');
+      console.log(token);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY); // jsonwebtoken mock 을 만들어줘서 jwt사용해도 mock으로 실행 됨.
+    });
+  });
+
   it.todo('sign');
+
+  describe('verify', () => {
+    it('should ', async () => {});
+  });
+
   it.todo('verify');
 });
