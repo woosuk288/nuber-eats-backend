23e137b238fddcc736f6c223a4f33ace6cdca021
양우석 <woosuk288@naver.com>
2020-12-10 22:51:11 +0900

    #8.3 sendVerificationEmail Test

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -32,6 +32,31 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {
+        console.log('I love you');
+      });
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify Your Email',
+        'email_confirm',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'username', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
+
   it.todo('sendEmail');
   it.todo('sendVerificationEmail');
 });
