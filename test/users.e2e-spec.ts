a639eb7d7a597eaeeb42bda19f197b31caedd1ff
양우석 <woosuk288@naver.com>
2020-12-13 18:29:42 +0900

    #9.2 Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -21,7 +23,36 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'woo@suk.com';
+    it('should ', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "woo@suk.com",
+              password: "123",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          console.log(res.body.ok);
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account aleady exists');
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('verifyEmail');
