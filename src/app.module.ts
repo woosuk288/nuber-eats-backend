4d4b817652d7d56e5ac1256dd822c9976a77d472
양우석 <woosuk288@naver.com>
2020-12-13 16:06:14 +0900

    #9.0 Setup part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,7 +33,7 @@ import { MailModule } from './mail/mail.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -54,7 +54,8 @@ import { MailModule } from './mail/mail.module';
       database: process.env.DB_DATABASE,
       entities: [User, Verification],
       synchronize: true,
-      logging: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
     }),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
