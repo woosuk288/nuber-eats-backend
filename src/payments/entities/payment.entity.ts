1f76459e402fcd9d2cc4bb7b51091993798e9019
양우석 <woosuk288@naver.com>
2021-01-06 01:25:04 +0900

    #13.3 createPayment part Two

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -10,7 +10,7 @@ import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 export class Payment extends CoreEntity {
   @Field()
   @Column()
-  transactionId: number;
+  transactionId: string;
 
   @Field(() => User)
   @ManyToOne(() => User, (user) => user.payments)
--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
 import { PaymentService } from './payments.service';
 
 @Resolver()
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation(() => CreatePaymentOutput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ) {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
 }
